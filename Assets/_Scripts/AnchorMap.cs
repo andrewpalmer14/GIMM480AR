83fc2478d2e595bc870cdc0e4e2dffb7a2d3f5b3
Andrew Palmer <andrewpal1@live.com>
2018-09-28 09:04:31 -0600

    ar
    asdf

--- Assets/_Scripts/AnchorMap.cs
+++ Assets/_Scripts/AnchorMap.cs
@@ -7,17 +7,17 @@ public class AnchorMap : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		/*Touch touch;
+		Touch touch;
 		if (Input.touchCount<1 || (touch=Input.GetTouch(0)).phase!=TouchPhase.Began) {
 			return;
 		}
 
-		Trackable hit;
+		TrackableHit hit;
 
 		TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinBounds | TrackableHitFlags.PlaneWithinPolygon | TrackableHitFlags.FeaturePointWithSurfaceNormal;
 		if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit)) {
 			var anchor = hit.Trackable.CreateAnchor(hit.Pose);
 			transform.position = anchor.transform.position;
-		}*/
+		}
 	}
 }
