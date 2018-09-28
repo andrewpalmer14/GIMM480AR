1458cd8a00312f7153744c9e5b3b2160a32cb522
Andrew Palmer <andrewpal1@live.com>
2018-09-27 20:27:38 -0600

    initial commit
    first map

---
+++ Assets/_Scripts/RemoveGameObjects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveGameObjects : MonoBehaviour {
+
+	public GameObject mapGameObject;
+
+	// Use this for initialization
+	void Start () {
+		// gameobjects that need to be removed: map - child - 17/23242/47860 - child - last(building - 1)
+		//										map - child - 17/23242/47861 - child - last(building - 1)
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
